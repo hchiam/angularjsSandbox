b3c2dc3eb2500b81466486e3c849c9bfd4432463
hchiam <hchiam@gmail.com>
2016-12-13 18:08:29 -0500

    add img, add filter, add variables

--- codecademy-example/example1/js/controllers/MainController.js
+++ codecademy-example/example1/js/controllers/MainController.js
@@ -1,4 +1,8 @@
 app.controller('MainController', ['$scope', function($scope) { 
   $scope.title = 'Top Sellers in Books';
   $scope.promo = "Promotions are Back!";
+  $scope.product = {
+    name: 'The Book of Trees',
+    price: 19
+  };
 }]);
