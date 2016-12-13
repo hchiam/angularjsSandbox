0225afcea8ae9713164ddc9d574093b260bb80af
hchiam <hchiam@gmail.com>
2016-12-13 17:07:54 -0500

    update notes & tutorial

--- codecademy-example/example1/js/controllers/MainController.js
+++ codecademy-example/example1/js/controllers/MainController.js
@@ -1,3 +1,4 @@
 app.controller('MainController', ['$scope', function($scope) { 
-  $scope.title = 'Top Sellers in Books'; 
+  $scope.title = 'Top Sellers in Books';
+  $scope.promo = "Promotions are Here!";
 }]);
