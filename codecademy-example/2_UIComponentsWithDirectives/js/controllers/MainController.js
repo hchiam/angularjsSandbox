3655505f3e65d27f68f16f15deb1222f2c038bfc
hchiam <hchiam@gmail.com>
2016-12-14 18:47:08 -0500

    add array of apps

--- codecademy-example/2_UIComponentsWithDirectives/js/controllers/MainController.js
+++ codecademy-example/2_UIComponentsWithDirectives/js/controllers/MainController.js
@@ -26,4 +26,20 @@ app.controller('MainController', ['$scope', function($scope) {
         developer: 'Forecast',
         price: 1.99
     };
+    
+    $scope.apps = [
+        {
+            icon: 'img/move.jpg',
+            title: 'MOVE',
+            developer: 'Move, Inc.',
+            price: 0.99
+        },
+        {
+            icon: 'img/shutterbugg.jpg',
+            title: 'Shutterbug',
+            developer: 'Chico Dusty',
+            price: 2.99
+        }
+    ];
+    
 }]);
