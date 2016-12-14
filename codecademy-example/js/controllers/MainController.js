6d39789813514c7730888d87057e37e9641d314c
hchiam <hchiam@gmail.com>
2016-12-13 19:37:50 -0500

    add likes and function

--- codecademy-example/js/controllers/MainController.js
+++ codecademy-example/js/controllers/MainController.js
@@ -1,4 +1,7 @@
-app.controller('MainController', ['$scope', function($scope) { 
+app.controller('MainController', ['$scope', function($scope) {
+  
+  /*CONTROLLER VARIABLES*/
+  
   $scope.title = 'Top Sellers in Books';
   $scope.promo = "Promotions are Back!";
   //$scope.product = {
@@ -11,21 +14,31 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'The Book of Trees',
       price: 19,
       pubdate: new Date('2014','03','08'),
-      cover: 'img/the-book-of-trees.jpg'
+      cover: 'img/the-book-of-trees.jpg',
+      likes: 0
     },
     {
       name: 'Program or be Programmed',
       price: 8,
       pubdate: new Date('2013','08','01'),
-      cover: 'img/program-or-be-programmed.jpg'
+      cover: 'img/program-or-be-programmed.jpg',
+      likes: 0
     },
     {
-      name: 'My Favourite Book'
+      name: 'My Favourite Book',
       //turns out you don't need to include all properties! the ng-repeat doesn't show something weird like "null"!
+      likes: 0
     },
     {
-      name: 'My Other Favourite Book'
+      name: 'My Other Favourite Book',
       //turns out you don't need to include all properties! the ng-repeat doesn't show something weird like "null"!
+      likes: 0
     }
   ];
+  
+  /*CONTROLLER FUNCTIONS*/
+  
+  $scope.plusOne = function(index){
+    $scope.products[index].likes += 1;
+  };
 }]);
