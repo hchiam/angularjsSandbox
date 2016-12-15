a4fe592330d63cc25aa580ea04163c83b048db11
hchiam <hchiam@gmail.com>
2016-12-14 19:10:21 -0500

    add ng-repeat

--- codecademy-example/2_UIComponentsWithDirectives/js/controllers/MainController.js
+++ codecademy-example/2_UIComponentsWithDirectives/js/controllers/MainController.js
@@ -1,32 +1,4 @@
 app.controller('MainController', ['$scope', function($scope) {
-    $scope.move = {
-        icon: 'img/move.jpg',
-        title: 'MOVE',
-        developer: 'MOVE, Inc.',
-        price: 0.99
-    };
-    
-    $scope.shutterbugg = {
-        icon: 'img/shutterbugg.jpg',
-        title: 'Shutterbugg',
-        developer: 'Chico Dusty',
-        price: 2.99
-    };
-    
-    $scope.gameboard = {
-        icon: 'img/gameboard.jpg',
-        title: 'Gameboard',
-        developer: 'Armando P.',
-        price: 1.99
-    };
-    
-    $scope.forecast = {
-        icon: 'img/forecast.jpg',
-        title: 'Forecast',
-        developer: 'Forecast',
-        price: 1.99
-    };
-    
     $scope.apps = [
         {
             icon: 'img/move.jpg',
@@ -39,7 +11,46 @@ app.controller('MainController', ['$scope', function($scope) {
             title: 'Shutterbug',
             developer: 'Chico Dusty',
             price: 2.99
+        },
+        {
+            icon: 'img/gameboard.jpg',
+            title: 'Angry Birds',
+            developer: 'Rovio Entertainment',
+            price: 0
+        },
+        {
+            icon: 'img/forecast.jpg',
+            title: 'made-up app',
+            developer: 'made-up developer',
+            price: 1000.00
         }
     ];
     
+    //$scope.move = {
+    //    icon: 'img/move.jpg',
+    //    title: 'MOVE',
+    //    developer: 'MOVE, Inc.',
+    //    price: 0.99
+    //};
+    //
+    //$scope.shutterbugg = {
+    //    icon: 'img/shutterbugg.jpg',
+    //    title: 'Shutterbugg',
+    //    developer: 'Chico Dusty',
+    //    price: 2.99
+    //};
+    //
+    //$scope.gameboard = {
+    //    icon: 'img/gameboard.jpg',
+    //    title: 'Gameboard',
+    //    developer: 'Armando P.',
+    //    price: 1.99
+    //};
+    //
+    //$scope.forecast = {
+    //    icon: 'img/forecast.jpg',
+    //    title: 'Forecast',
+    //    developer: 'Forecast',
+    //    price: 1.99
+    //};
 }]);
--- codecademy-example/2_UIComponentsWithDirectives/js/directives/appInfo.js
+++ codecademy-example/2_UIComponentsWithDirectives/js/directives/appInfo.js
@@ -3,7 +3,7 @@ This code will "teach" the browser a new HTML element/directive called <app-info
 appInfo.html shows the template sub-elements for <app-info>
 */
 
-app.directive('appInfo', function() {
+app.directive('appInfo',function(){
     return {
         restrict: 'E', // restrict = how the element/directive will be used ; 'E' = as a new HTML element
         scope: { // scope = means we will pass info into this element/directive, such as attributes
