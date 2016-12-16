ed18c6e21850ebff2569333577708035a558001e
hchiam <hchiam@gmail.com>
2016-12-16 16:25:19 -0500

    comment success and error

--- codecademy-example/3_ServicesServerComms/js/services/forecast.js
+++ codecademy-example/3_ServicesServerComms/js/services/forecast.js
@@ -2,9 +2,9 @@ app.factory('forecast', ['$http', function($http) {
     // call a url ("https://...") to get data (".json")
     return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json')
         .success(function(data) {
-            return data;
+            return data; // if success
         })
-            .error(function(err) {
-                return err;
-            });
+        .error(function(err) {
+            return err; // if error
+        });
 }]);
