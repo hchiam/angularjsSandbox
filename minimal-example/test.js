ef09020f17d3c943ef4bbec48721348a237f65a8
hchiam <hchiam@gmail.com>
2016-12-26 21:05:42 -0500

    h1 -> p ; comment module and controller

--- minimal-example/test.js
+++ minimal-example/test.js
@@ -1,5 +1,7 @@
+// module:
 var app = angular.module("myApp", []);
 
+// controller:
 app.controller('MainController', ['$scope', function($scope) {
     $scope.placeholderText = 'This is text that automatically filled the placeholder!';
 }]);
